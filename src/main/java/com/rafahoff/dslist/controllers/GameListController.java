617242be98a9e5d9f0102686a80d8ea10db1e8b3
Rafa-Hoff <rafahoff32@gmail.com>
2023-11-15 11:42:17 -0400

    Query SQL, projection, search games by list

--- src/main/java/com/rafahoff/dslist/controllers/GameListController.java
+++ src/main/java/com/rafahoff/dslist/controllers/GameListController.java
@@ -1,9 +1,12 @@
 package com.rafahoff.dslist.controllers;
 
 import com.rafahoff.dslist.dto.GameListDTO;
+import com.rafahoff.dslist.dto.GameMinDTO;
 import com.rafahoff.dslist.services.GameListService;
+import com.rafahoff.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +19,18 @@ public class GameListController {
     @Autowired
     private GameListService gameListService;
 
+    @Autowired
+    private GameService gameService;
+
     @GetMapping
     public List<GameListDTO> findAll(){
         List<GameListDTO> result = gameListService.findAll();
+        return result;
+    }
+
+    @GetMapping(value = "/{listId}/games")
+    public List<GameMinDTO> findByList(@PathVariable Long listId){
+        List<GameMinDTO> result = gameService.findByList(listId);
         return result;
     }
 }
